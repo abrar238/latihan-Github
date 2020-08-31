c37b5ea2b1336aa2d9c8b8e7b3f9eff0c6c8c174
abrar238 <abrar.putra@gmail.com>
2020-08-31 16:50:02 +0700

    baru

--- php2/home.php
+++ php2/home.php
@@ -1,6 +1,97 @@
 <?php
 if ($_GET['hal']=="home") {
-	echo "Selamat Datang di Home";
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h3>Dashbord</h3>
+  <div class="row">
+    <div class="col-sm">
+		  <div class="card text-white bg-info mb-3" style="width: 12rem;">
+		    <div class="card-header">Info</div>
+		    <div class="card-body">
+		      <p class="card-text">Some quick example text to build on the card title.</p>
+		  </div>
+			<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo1">Simple collapsible
+			</button>
+			<div id="demo1" class="collapse">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</div>
+    </div>
+	</div>
+    <div class="col-sm">
+      	<div class="card text-white bg-success mb-3" style="width: 12rem;">
+		  <div class="card-header">Succes</div>
+		  <div class="card-body">
+		    <p class="card-text">Some quick example text to build on the card title.</p>
+		  </div>
+		  <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#demo2">Simple collapsible
+			</button>
+			<div id="demo2" class="collapse">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</div>
+		</div>
+    </div>
+    <div class="col-sm">
+      <div class="card text-white bg-warning mb-3" style="width: 12rem;">
+	    <div class="card-header">Warning</div>
+	    <div class="card-body">
+	      <p class="card-text">Some quick example text to build on the card title.</p>
+	  </div>
+	  		<button type="button" class="btn btn-warning text-white" data-toggle="collapse" data-target="#demo4">Simple collapsible
+			</button>
+			<div id="demo4" class="collapse">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</div>
+    </div>
+	</div>
+     <div class="col-sm">
+      <div class="card text-white bg-danger mb-3" style="width: 12rem;">
+	    <div class="card-header">Danger</div>
+	    <div class="card-body">
+	      <p class="card-text">Some quick example text to build on the card title.</p>
+	  </div>
+	 	    <button type="button" class="btn btn-danger" data-toggle="collapse" data-target="#demo3">Simple collapsible
+			</button>
+			<div id="demo3" class="collapse">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</div>
+	</div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">      
+    	<div class="card" style="width: 24rem;">
+    	  <div class="card-header">Bar Chart</div>
+		  <img src="img/bc.png" class="card-img-top" alt="bar chart">
+		</div>
+    </div>
+    <div class="col">   
+    	 <div class="card" style="width: 24rem;">
+    	  <div class="card-header">Pie CHart</div>
+		  <img src="img/pc.png" class="card-img-top" alt="pie chart">
+		</div>
+    </div>
+  </div>
+</body>
+</html>
+
+
+<?php
 } elseif ($_GET['hal']=="kontak") {
 	include "konten/buku_telp/detail.php";
 }elseif ($_GET['hal']=="register") {
--- php2/menus.php
+++ php2/menus.php
@@ -1,8 +1,36 @@
- <ul class="list-group">
-  <a href="?hal=home"><li class="list-group-item active">HOME</li></a>
-  <a href="?hal=kontak"><li class="list-group-item">DAFTAR KONTAK</li></a>
-  <a href="?hal=register"><li class="list-group-item">REGISTER</li></a>
-  <a href="?hal=edit"><li class="list-group-item">EDIT KONTAK</li></a>
-  <a href="?hal=upload"><li class="list-group-item">UPLOAD FILES</li></a>
-  <a href="?hal=gambar"><li class="list-group-item">UPLOAD GAMBAR</li></a>
-</ul>
+<div class="wrapper">
+<nav id="sidebar">
+  <ul class="list-unstyled componet bg-dark">
+    <li>
+      <a href="?hal=home" class="nav-link bg-dark text-white">Dashbord</a>
+    </li>
+    <hr>
+    <li>
+      <a href="#member" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link bg-dark text-white">Member</a>
+        <ul class="collapse list-unstyled" id="member">
+          <li>
+            <a href="?hal=kontak" class="nav-link bg-dark text-white">Detail</a>
+          </li>
+          <li>
+            <a href="?hal=register" class="nav-link bg-dark text-white">Register</a>
+          </li>
+          <li>
+            <a href="?hal=edit" class="nav-link bg-dark text-white">Edit Member</a>
+          </li>
+        </ul>
+    </li>
+  <li>
+    <a href="#upload" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link bg-dark text-white">Upload</a>
+    <ul class="collapse list-unstyled" id="upload">
+    <li>
+      <a href="?hal=upload" class="nav-link bg-dark text-white">Upload Pdf</a>
+    </li>
+    <li>
+      <a href="?hal=gambar" class="nav-link bg-dark text-white"> Upload Img</a>
+    </li>
+    </ul>
+  </li>
+  </ul>
+</nav>
+</div>
+
